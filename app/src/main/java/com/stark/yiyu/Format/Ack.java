a4c8859305e03c336806290ff697174eda2eccb6
Stark <yiqinlin@foxmail.com>
2017-06-26 18:16:30 +0800

    origin

---
+++ app/src/main/java/com/stark/yiyu/Format/Ack.java
@@ -0,0 +1,13 @@
+package com.stark.yiyu.Format;
+
+/**
+ * Created by Stark on 2017/2/11.
+ */
+public class Ack {
+    public String SrcId;
+    public String DesId;
+    public String BackMsg;
+    public String MsgCode;
+    public boolean Flag;
+    public int Error;
+}
